a8f13efc2ae81697874564e5ddc03483913234f3
Servando <swyswy214@gmail.com>
2021-10-31 15:12:00 -0500

    install axios, call api, added searchWord and toggleCards states

--- dictionary/src/components/cards/cards.js
+++ dictionary/src/components/cards/cards.js
@@ -1,104 +1,124 @@
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 
-export default function Cards() {
-    return (
-        <Row xs={1} md={2}>
-            <Col>
-                <Card className="cardsCenter" style={{ width: '30rem' }}>
-                    <Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>
-                                <Card.Title>Word Title</Card.Title>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    <p>Part of speech: noun</p>
-                                    Definition 1: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Definition 2: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Definition 3: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Example: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col>
-                <Card className="cardsCenter" style={{ width: '30rem' }}>
-                    <Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>
-                                <Card.Title>Word Title</Card.Title>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    <p>Part of speech: noun</p>
-                                    Definition 1: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Definition 2: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Definition 3: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <Card.Text>
-                                    Example: Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </ListGroupItem>
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
-            </Col>
-            {/* <Col>
-                <Card className="cardsCenter" style={{ width: '30rem' }}>
-                    <Card.Body>
-                        <Card.Title>Word Title</Card.Title>
-                        <Card.Text>
-                            Definition 1: Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroupItem>Cras justo odio</ListGroupItem>
-                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                </Card>
-            </Col> */}
-        </Row>
-    );
+export default function Cards(props) {
+
+  return (
+    <Col>
+      <Card className="cardsCenter" style={{ width: "30rem" }}>
+        <Card.Body>
+          <Card.Title>{props.word}</Card.Title>
+          <Card.Text>
+            Definition 1: Some quick example text to build on the card title and
+            make up the bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Cras justo odio</ListGroupItem>
+          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </Col>
+    // <Row xs={1} md={2} className="center">
+    //     <Col>
+    //         <Card style={{ width: '30rem' }}>
+    //             <Card.Body>
+    //                 <ListGroup className="list-group-flush">
+    //                     <ListGroupItem>
+    //                         <Card.Title>Word Title</Card.Title>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             <p>Part of speech: noun</p>
+    //                             Definition 1: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Definition 2: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Definition 3: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Example: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                 </ListGroup>
+    //             </Card.Body>
+    //         </Card>
+    //     </Col>
+    //     <Col>
+    //         <Card className="cardsCenter" style={{ width: '30rem' }}>
+    //             <Card.Body>
+    //                 <ListGroup className="list-group-flush">
+    //                     <ListGroupItem>
+    //                         <Card.Title>Word Title</Card.Title>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             <p>Part of speech: noun</p>
+    //                             Definition 1: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Definition 2: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Definition 3: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                     <ListGroupItem>
+    //                         <Card.Text>
+    //                             Example: Some quick example text to build on the card title and make up the bulk of
+    //                             the card's content.
+    //                         </Card.Text>
+    //                     </ListGroupItem>
+    //                 </ListGroup>
+    //             </Card.Body>
+    //         </Card>
+    //     </Col>
+    //  <Col>
+    //     <Card className="cardsCenter" style={{ width: '30rem' }}>
+    //         <Card.Body>
+    //             <Card.Title>Word Title</Card.Title>
+    //             <Card.Text>
+    //                 Definition 1: Some quick example text to build on the card title and make up the bulk of
+    //                 the card's content.
+    //             </Card.Text>
+    //         </Card.Body>
+    //         <ListGroup className="list-group-flush">
+    //             <ListGroupItem>Cras justo odio</ListGroupItem>
+    //             <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    //             <ListGroupItem>Vestibulum at eros</ListGroupItem>
+    //         </ListGroup>
+    //         <Card.Body>
+    //             <Card.Link href="#">Card Link</Card.Link>
+    //             <Card.Link href="#">Another Link</Card.Link>
+    //         </Card.Body>
+    //     </Card>
+    // </Col>
+    // </Row>
+  );
 }
--- dictionary/src/screens/Home.js
+++ dictionary/src/screens/Home.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navigation from "../components/navbar/nav";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -7,8 +8,24 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Cards from "../components/cards/cards";
+import axios from "axios";
 
 export default function Home() {
+  const [searchWord, setSearchWord] = useState("");
+  const [toggleCards, setToggleCard] = useState(false);
+  let baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+  const handleChange = (event) => {
+    setSearchWord(event.target.value);
+  };
+
+  const submit = () => {
+    axios.get(baseUrl + searchWord).then(function (response) {
+      console.log(response.data)
+    })
+    setToggleCard(true);
+  };
+
   return (
     <div>
       <Navigation />
@@ -19,17 +36,25 @@ export default function Home() {
             <Form.Label>Search a word</Form.Label>
             <InputGroup className="mb-3">
               <FormControl
-                placeholder="Enter word"
+                placeholder="enter word"
+                value={searchWord}
+                onChange={handleChange}
               />
-              <Button variant="dark" id="button-addon2">
+              <Button variant="dark" id="button-addon2" onClick={submit}>
                 Search
               </Button>
             </InputGroup>
           </Col>
         </Row>
 
-        <Cards />
+        <Row className="center">
+          {toggleCards && <Cards word={searchWord} />}
 
+          {toggleCards && <Cards word={searchWord} />}
+        </Row>
+        <div className="footer">
+          <span>https://dictionaryapi.dev/</span>
+        </div>
       </Container>
     </div>
   );
